30b413707d680fb480f94c0c6b95be83e4a8c9dd
felixreynosob <engfelixreynoso@gmail.com>
2019-11-05 23:48:16 -0400

    bootcamp project, expanding on classes

---
+++ week_2/W2D4/felix_reynoso_bootcamp_project/lib/bootcamp.rb
@@ -0,0 +1,73 @@
+class Bootcamp
+
+    def initialize(name, slogan, student_capacity)
+        @name = name
+        @slogan = slogan
+        @student_capacity = student_capacity
+        @teachers = []
+        @students = []
+        @grades = Hash.new { |hash, k| hash[k] = [] }
+    end
+
+    def name
+        @name
+    end
+
+    def slogan
+        @slogan
+    end
+
+    def teachers
+        @teachers
+    end
+
+    def students
+        @students
+    end
+
+    def hire(teacher)
+        @teachers.push(teacher)
+    end
+
+    def enroll(student)
+        if @students.length < @student_capacity
+            @students << student
+            return true
+        else
+            return false
+        end
+    end
+
+    def enrolled?(student)
+        @students.include?(student)
+    end
+
+    def student_to_teacher_ratio
+        ratio = @students.length / @teachers.length.to_f
+        return ratio.floor()
+    end
+
+    def add_grade(student, grade)
+        if enrolled?(student)
+            @grades[student] << grade
+            return true
+        else
+            return false
+        end
+    end
+
+    def num_grades(student)
+        return @grades[student].length
+    end
+
+    def average_grade(student)
+        if !enrolled?(student)
+            return nil
+        elsif !@grades.has_key?(student)
+            return nil
+        else
+        (@grades[student].inject {|sum, el| sum += el} / @grades[student].length.to_f).floor()
+        end
+    end
+
+end
