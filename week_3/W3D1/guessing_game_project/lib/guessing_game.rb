ca5262537b7186e24d4ce88d8d9dc85ad1fa6f27
felixreynosob <engfelixreynoso@gmail.com>
2019-11-11 11:04:04 -0400

    Final solution for guessing game project, App Academy

--- week_3/W3D1/guessing_game_project/lib/guessing_game.rb
+++ week_3/W3D1/guessing_game_project/lib/guessing_game.rb
@@ -1,3 +1,32 @@
 class GuessingGame
+    def initialize(min, max)
+        @secret_num = rand(min..max)
+        @num_attempts = 0 
+        @game_over = false
+    end
 
+    def game_over?
+        @game_over
+    end
+    
+    def num_attempts
+        @num_attempts
+    end
+
+    def check_num(num)
+        if num == @secret_num
+            @game_over = true
+            puts "You win"
+        elsif num < @secret_num
+            puts "Too small"
+        else
+            puts "Too big"
+        end
+        @num_attempts += 1
+    end
+
+    def ask_user
+        print "Enter a number:"
+        check_num(gets.chomp.to_i)
+    end  
 end
