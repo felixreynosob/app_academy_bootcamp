4b30c1552d25ea3c6603f41a748ee6ec27ac9b25
felixreynosob <engfelixreynoso@gmail.com>
2019-11-11 14:58:43 -0400

    Hotel project, used two classes to simulate simple guest management actions.

--- week_3/W3D2/hotel_project/lib/hotel.rb
+++ week_3/W3D2/hotel_project/lib/hotel.rb
@@ -2,4 +2,58 @@ require_relative "room"
 
 class Hotel
   
+    def initialize(name, hash_rooms)
+        @name = name
+        @rooms = {}
+        hash_rooms.each { |k, v| @rooms[k] = Room.new(v) }
+    end
+
+
+    def name
+        tmp = @name.split(" ")
+        tmp.each {|word| word.capitalize! }
+        tmp.join(" ")
+    end
+
+
+    def rooms
+        @rooms
+    end
+
+
+    def room_exists?(room_name)
+        if @rooms.has_key?(room_name)
+            true
+        else
+            false
+        end
+    end
+
+
+    def check_in(guest, room_name)
+        if room_exists?(room_name)
+            if @rooms[room_name].add_occupant(guest) == true
+                puts 'check in successful'
+            else
+                puts'sorry, room is full'
+            end
+        else
+            puts 'sorry, room does not exist'
+        end
+    end
+
+
+    def has_vacancy?
+        if @rooms.any? { | k, v| !@rooms[k].full? }
+            true
+        else
+            false
+        end
+    end
+
+
+    def list_rooms
+        @rooms.each { |k, v| puts "#{k} #{@rooms[k].available_space}" }
+    end
+
 end
--- week_3/W3D2/hotel_project/lib/room.rb
+++ week_3/W3D2/hotel_project/lib/room.rb
@@ -1,3 +1,42 @@
 class Room
 
+    def initialize(capacity)
+        @capacity = capacity
+        @occupants = []
+    end
+
+
+    def capacity
+        @capacity
+    end
+
+
+    def occupants
+        @occupants
+    end
+
+
+    def full?
+        if @occupants.length < @capacity
+            false
+        else
+            true
+        end
+    end
+
+
+    def available_space
+        @capacity - @occupants.length
+    end
+
+
+    def add_occupant(name)
+        if !full?
+            @occupants << name
+            true
+        else
+            false
+        end
+    end
+
 end
