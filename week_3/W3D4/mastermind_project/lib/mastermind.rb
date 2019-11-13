8dacda7b8e624aca8bb3e566469e80da8178af5b
felixreynosob <engfelixreynoso@gmail.com>
2019-11-12 21:56:26 -0400

    Finished mastermind project, used encapsulation, abstraction, created multiple classes. Learned about Syntactic sugar and defined instance and class methods using operators as names such as ==, [], etc.

--- week_3/W3D4/mastermind_project/lib/mastermind.rb
+++ week_3/W3D4/mastermind_project/lib/mastermind.rb
@@ -2,35 +2,31 @@ require_relative "code"
 
 class Mastermind
 
-    attr_reader :secret_code
-
-
     def initialize(length)
         @secret_code = Code.random(length)
     end
 
+
     def print_matches(code_instance)
-        puts "exact matches: #{code_instance.num_exact_matches(@secret_code)}"
-        puts "near matches: #{code_instance.num_near_matches(@secret_code)}"  
+        puts "exact matches: #{@secret_code.num_exact_matches(code_instance)}"
+        puts "near matches: #{@secret_code.num_near_matches(code_instance)}"
+
     end
 
+
     def ask_user_for_guess
-        #puts 'Enter a code'
+        puts "Enter a code"
         str = gets.chomp
         n_instance = Code.from_string(str)
         print_matches(n_instance)
-        
+        i = 0
+        while i < @secret_code.length
+           return false if @secret_code[i] != n_instance.pegs[i]
+           i += 1
+        end
+        true
     end
 
 end
-
-
-
-# m_obj = Mastermind.new(4)
-# p m_obj.secret_code
-# p "--"
-# p m_obj.print_matches(Code.new(["Y", "B", "G", "R"]))
-
-
 
 
