afb806ce506a4941ab01863e549baa15cd3f7b46
felixreynosob <engfelixreynoso@gmail.com>
2019-11-30 10:44:59 -0400

    Finished enumerables ruby exercises

--- ruby/enumerables.rb
+++ ruby/enumerables.rb
@@ -45,18 +45,60 @@ class Array
     end
 
 
+    def my_zip(*data)
+        zipped = []
+        self.my_each { |el| zipped << [el] }
+        
+        data.my_each do |sub_arr|
+            i = 0
+            while i < self.length
+                zipped[i] << sub_arr[i]
+                i += 1
+            end
+        end
+
+        zipped
+    end
+
+
+    def my_rotate(times=1)
+        rot = self.join("")
+        if times < 0
+            times *= -1
+            times.times { rot = rot[-1] + rot[0...-1] }
+        else 
+            times.times { rot = rot[1..-1] + rot[0] }
+        end
+        rot.split("")
+    end
+
+
+    def my_join(separator='')
+        joined = String.new
+        i = 0
+        self.my_each do |el|
+            if i == (self.length - 1)
+                joined += el.to_s
+                break
+            end
+            joined += (el.to_s + separator)
+            i += 1
+        end
+        joined
+    end
+
+
+    def my_reverse
+        i = self.length
+        reversed = []
+        reversed.push(self[i-=1]) while i > 0 
+        reversed
+    end
+
 end
 
 
-p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten
 
 
-# def my_flatten(data)
-#     return [data] if !data.is_a?(Array)
-
-#     flattnened = []
-#     data.each { |el| flattnened += my_flatten(el) } 
-#     flattnened
-# end
 
 
