bd400f3179f9025f25bf99b8524b159d944b3304
felixreynosob <engfelixreynoso@gmail.com>
2019-12-08 22:43:03 -0400

    .

--- software_engineering_foundations/week_4/W4D3/recap_exercise_2/lib/problems.rb
+++ software_engineering_foundations/week_4/W4D3/recap_exercise_2/lib/problems.rb
@@ -81,7 +81,7 @@ class Array
         sorted = false
         while !sorted
             sorted = true
-
+            
             (0...self.length - 1).each do |i|
                 if prc.call(self[i], self[i + 1]) == 1
                     self[i], self[i + 1] = self[i + 1], self[i]
