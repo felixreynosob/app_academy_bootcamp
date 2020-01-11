aeb848ab94c9ba8a9b0a0a45d33894ca80ba2bab
felixreynosob <engfelixreynoso@gmail.com>
2020-01-11 11:32:06 -0400

    Completed queue class implementation

---
+++ ruby/5.data_structures/queue.rb
@@ -0,0 +1,17 @@
+class Queue
+    def initialize
+        @ivar = []
+    end
+
+    def enqueue(el)
+        @ivar.push(el)
+    end
+
+    def dequeue
+        @ivar.shift
+    end
+
+    def peek
+        @ivar.first
+    end
+end
