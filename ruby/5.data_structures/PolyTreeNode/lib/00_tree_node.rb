b95f05144fccd865f627bfcf1e30104f338c9515
felixreynosob <engfelixreynoso@gmail.com>
2020-01-11 18:08:53 -0400

    Completed first part, successfully implemented PolyTreeNode Class

--- ruby/5.data_structures/PolyTreeNode/lib/00_tree_node.rb
+++ ruby/5.data_structures/PolyTreeNode/lib/00_tree_node.rb
@@ -1,3 +1,39 @@
 class PolyTreeNode
 
+    attr_reader :children, :parent, :value
+
+    
+    def initialize(given_value)
+        @value = given_value
+        @parent = nil
+        @children = []
+    end
+
+
+    def parent=(new_parent)
+        if new_parent == nil 
+            @parent = nil
+        elsif @parent == nil 
+            @parent = new_parent
+            if !new_parent.children.include?(self) 
+                new_parent.children.push(self) 
+            end  
+        elsif @parent != nil
+            @parent.children.delete(self)
+            @parent = new_parent
+            @parent.children.push(self)
+        end
+    end
+
+    def add_child(node)
+        node.parent= self
+        @children.push(node) unless @children.include?(node)
+    end
+
+
+    def remove_child(node)
+        node.parent = nil
+        raise error if !@children.include?(node)
+        @children.delete(node) 
+    end
 end
