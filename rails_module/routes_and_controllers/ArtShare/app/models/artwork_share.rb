cc9e1730e490525d89962968b2e2459696220f91
felixreynosob <engfelixreynoso@gmail.com>
2020-04-05 14:44:06 -0400

    ArtShare App

--- rails_module/routes_and_controllers/ArtShare/app/models/artwork_share.rb
+++
@@ -1,7 +0,0 @@
-class ArtworkShare < ApplicationRecord
-    validates :artwork_id, uniqueness: { scope: :viewer_id }
-    validates :favorite, inclusion: { in: [true, false] }
-  
-    belongs_to :artwork
-    belongs_to :viewer, class_name: 'User'
-end
